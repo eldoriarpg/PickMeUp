108a12f67b0ad13633cb790e3cf98c6f70d0e170
Florian Fülling <mail@chojo.de>
2021-01-10 00:24:02 +0100

    use default about.

--- src/main/java/de/eldoria/pickmeup/commands/PickMeUpCommand.java
+++ src/main/java/de/eldoria/pickmeup/commands/PickMeUpCommand.java
@@ -1,12 +1,13 @@
 package de.eldoria.pickmeup.commands;
 
 import de.eldoria.eldoutilities.simplecommands.EldoCommand;
+import de.eldoria.eldoutilities.simplecommands.commands.DefaultAbout;
 import org.bukkit.plugin.Plugin;
 
 public class PickMeUpCommand extends EldoCommand {
     public PickMeUpCommand(Plugin plugin) {
         super(plugin);
         registerCommand("reload", new Reload(plugin));
-        registerCommand("about", new About(plugin));
+        registerCommand("about", new DefaultAbout(plugin));
     }
 }

1b7d3b71fc4ee7c77d6710156a1ec23eb8d20380
Florian Fülling <mail@chojo.de>
2021-01-10 00:24:34 +0100

    minor changes

--- src/main/java/de/eldoria/pickmeup/util/Permissions.java
+++ src/main/java/de/eldoria/pickmeup/util/Permissions.java
@@ -2,10 +2,13 @@ package de.eldoria.pickmeup.util;
 
 import org.bukkit.entity.EntityType;
 
-public class Permissions {
+public final class Permissions {
     public static final String BASE = "pickmeup.";
     public static final String RELOAD = BASE + "reload";
     public static final String BYPASS_NOSTACK = BASE + "bypass.nostack";
+
+    private Permissions() {
+    }
 
     public static String getPickUpPermission(EntityType type) {
         return BASE + "pickup." + type.name().toLowerCase();
